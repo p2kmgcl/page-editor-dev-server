44734052b196a53c59cc5e6289c26e333ffda3b0
Pablo Molina <contact@pablomolina.me>
2020-03-20 10:58:30 +0100

    Remove version number from plugin dep

--- src/mock/liferay.js
+++ src/mock/liferay.js
@@ -1,20 +1,20 @@
 export const DYNAMIC_MODULES = {
-  'layout-content-page-editor-web@2.0.24/page_editor/plugins/comments/index': import(
+  '/page_editor/plugins/comments/index': import(
     'page_editor/plugins/comments/index'
   ),
-  'layout-content-page-editor-web@2.0.24/page_editor/plugins/page-structure/index': import(
+  '/page_editor/plugins/page-structure/index': import(
     'page_editor/plugins/page-structure/index'
   ),
-  'layout-content-page-editor-web@2.0.24/page_editor/plugins/contents/index': import(
+  '/page_editor/plugins/contents/index': import(
     'page_editor/plugins/contents/index'
   ),
-  'layout-content-page-editor-web@2.0.24/page_editor/plugins/widgets/index': import(
+  '/page_editor/plugins/widgets/index': import(
     'page_editor/plugins/widgets/index'
   ),
-  'layout-content-page-editor-web@2.0.24/page_editor/plugins/experience/index': import(
+  '/page_editor/plugins/experience/index': import(
     'page_editor/plugins/experience/index'
   ),
-  'layout-content-page-editor-web@2.0.24/page_editor/plugins/fragments/index': import(
+  '/page_editor/plugins/fragments/index': import(
     'page_editor/plugins/fragments/index'
   ),
 };
@@ -34,7 +34,16 @@ window.Liferay = {
 
   Loader: {
     require: (deps, cb, errorCb) => {
-      Promise.all(deps.map(dep => DYNAMIC_MODULES[dep]))
+      Promise.all(
+        deps
+          .map(dep =>
+            dep.replace(
+              /layout-content-page-editor-web@[0-9]+\.[0-9]+\.[0-9]+/g,
+              '',
+            ),
+          )
+          .map(dep => DYNAMIC_MODULES[dep]),
+      )
         .then(modules => {
           if (modules.some(module => !module)) {
             throw new Error('Some deps not found\n' + deps.join('\n'));
