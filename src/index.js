2fc47d78a55f11d742068917fc810006b1261989
Sandro Chinea <sandrochinea@gmail.com>
2020-06-04 11:36:57 +0100

    Weeeee

--- src/index.js
+++ src/index.js
@@ -28,6 +28,9 @@ const getRemoteFile = (file) => path.resolve(file);
 const BABEL_PLUGINS = [
   '@babel/plugin-proposal-export-namespace-from',
   '@babel/plugin-proposal-class-properties',
+  '@babel/plugin-proposal-nullish-coalescing-operator',
+  '@babel/plugin-proposal-object-rest-spread',
+  '@babel/plugin-proposal-optional-chaining',
   getLocalDep('react-refresh/babel'),
 ];
 
