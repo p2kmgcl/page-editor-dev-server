e6b7304f3c57965685d2bae9d5ce693320a14ce8
Pablo Molina <contact@pablomolina.me>
2020-06-11 11:30:20 +0200

    Fix iframe selector

--- src/build-page.js
+++ src/build-page.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-const login = async (page) => {
+const login = async page => {
   await page.goto(
     'http://localhost:8080/web/guest/home?p_p_id=com_liferay_login_web_portlet_LoginPortlet&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&saveLastPath=false&_com_liferay_login_web_portlet_LoginPortlet_mvcRenderCommandName=%2Flogin%2Flogin',
   );
@@ -29,7 +29,7 @@ const getGetDisplayContext = (page, url) =>
     }
   };
 
-module.exports = async (masterPage) => {
+module.exports = async masterPage => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
@@ -42,11 +42,11 @@ module.exports = async (masterPage) => {
     'http://localhost:8080/group/guest/~/control_panel/manage?p_p_id=com_liferay_layout_admin_web_portlet_GroupPagesPortlet&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_mvcPath=%2Fselect_layout_page_template_entry.jsp&_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_redirect=%2Fgroup%2Fguest%2F~%2Fcontrol_panel%2Fmanage%3Fp_p_id%3Dcom_liferay_layout_admin_web_portlet_GroupPagesPortlet%26p_p_lifecycle%3D0%26p_p_state%3Dmaximized%26p_v_l_s_g_id%3D20121%26p_p_auth%3DB9JHzlM8&_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_backURL=%2Fgroup%2Fguest%2F~%2Fcontrol_panel%2Fmanage%3Fp_p_id%3Dcom_liferay_layout_admin_web_portlet_GroupPagesPortlet%26p_p_lifecycle%3D0%26p_p_state%3Dmaximized%26p_v_l_s_g_id%3D20121%26p_p_auth%3DB9JHzlM8&_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_groupId=20121&_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_selPlid=0&_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_privateLayout=false&_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_selectedTab=basic-templates&p_p_auth=B9JHzlM8',
   );
 
-  await page.$$('.add-layout-action-option').then(async (handles) => {
+  await page.$$('.add-layout-action-option').then(async handles => {
     const tuples = await Promise.all(
-      handles.map(async (handle) => [
+      handles.map(async handle => [
         handle,
-        await handle.evaluate((element) => element.textContent.trim()),
+        await handle.evaluate(element => element.textContent.trim()),
       ]),
     );
 
@@ -62,9 +62,9 @@ module.exports = async (masterPage) => {
   });
 
   await page.waitFor(1000);
-  await page.waitForSelector('.dialog-iframe-node');
+  await page.waitForSelector('iframe');
 
-  await page.$eval('.dialog-iframe-node', async (f) => {
+  await page.$eval('iframe', async f => {
     const fillForm = async () => {
       if (!f || !f.contentDocument) return;
 
@@ -76,7 +76,7 @@ module.exports = async (masterPage) => {
       if (!input || !button) return setTimeout(fillForm, 1000);
 
       input.value = 'AutoContent';
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+      await new Promise(resolve => setTimeout(resolve, 1000));
       button.click();
 
       return setTimeout(fillForm, 1000);
